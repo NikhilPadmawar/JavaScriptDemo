e6d0b664d37e14d2a44d285619b0d07d2331d67c
Nikhil <padmawar.nikhil@gmail.com>
2018-06-24 11:43:10 +0530

    demoJS

---
+++ demo.js
@@ -0,0 +1,9 @@
+/*var person={
+	name:'nikhil',
+	age:23
+}
+
+for(item in person)
+{
+	alert(item);
+}*/
---
+++ index.js
@@ -0,0 +1,26 @@
+/*var num=0;
+
+outerlabel:for(var i=0;i<10;i++)
+{
+	for(var j=0;j<10;j++)
+	{
+		if(i==5 && j==5)
+		{
+			continue outerlabel;
+		}
+		
+	}
+	num++;
+}
+alert(num);*/
+/*var c=[2,5,70,8,90];
+console.log(c.sort(say));
+
+function say(v1,v2){
+	return v1-v2;
+}*/
+
+var Str='Hello,world,!';
+console.log(Str);
+var Strr=Str.split(',');
+console.log(Strr);
